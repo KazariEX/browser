2a4de9ae0ea93b93988be267ead3600858d59fda
KazariEX <1364035137@qq.com>
2024-03-19 17:25:42 +0800

    feat: 添加浏览器列表项

---
+++ components/BrowserItem.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+    import type { ParsedContent } from "@nuxt/content/types";
+
+    defineProps<{
+        data: {
+            title: string,
+            icon: string
+        } & ParsedContent
+    }>();
+</script>
+
+<template>
+    <div class="flex gap-4 p-4 b-1 b-solid b-gray rd-4">
+        <img class="w-21" :src="data.icon" :alt="data.title"/>
+        <div flex="~ col justify-between 1">
+            <nuxt-link class="text-8" :to="data._path">{{ data.title }}</nuxt-link>
+            <nuxt-link class="ml-auto text-gray hover-text-blue" flex="~ gap-1 items-center justify-end" :to="data.official" target="_blank">
+                <span>前往官网</span>
+                <icon name="pajamas:external-link"/>
+            </nuxt-link>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,12 @@
 </script>
 
 <template>
-    <h1 class="text-32">浏览器知识构建</h1>
+    <main class="max-w-320 mx-auto">
+        <h1 class="mt-32 mb-16 text-32">浏览器知识构建</h1>
+        <ContentList v-slot="{ list }" path="/posts">
+            <div grid="~ cols-[repeat(auto-fill,minmax(244px,1fr))] gap-4">
+                <BrowserItem v-for="item in list" :data="item"/>
+            </div>
+        </ContentList>
+    </main>
 </template>
