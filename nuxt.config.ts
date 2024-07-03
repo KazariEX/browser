59cc83b8374802c0fc4625c48a7a0b093e98344f
KazariEX <1364035137@qq.com>
2024-07-03 16:39:56 +0800

    chore: 迁移至 Nuxt 4 目录结构

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,9 @@ export default defineNuxtConfig({
     devtools: {
         enabled: true
     },
+    future: {
+        compatibilityVersion: 4
+    },
     routeRules: {
         "/slide": {
             ssr: false
